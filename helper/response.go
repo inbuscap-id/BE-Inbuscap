b35ad07032052011c1b8cae05db1c89a3c694d20
Abdul Malik <malik.the5th@gmail.com>
2024-04-08 14:05:52 +0700

    conflict resolved

--- helper/response.go
+++ helper/response.go
@@ -40,22 +40,3 @@ func ResponseFormatArray(status int, message any, data ...any) (int, map[string]
 	result["pagination"] = data[1]
 	return status, result
 }
-
-type Pagination struct {
-	Page       int `json:"page"`
-	PageSize   int `json:"page_size"`
-	TotalPages int `json:"total_pages"`
-}
-
-func ResponseFormatArray(status int, message any, data ...any) (int, map[string]any) {
-	result := map[string]any{
-		"code":    status,
-		"message": message,
-	}
-
-	if len(data) > 0 {
-		result["data"] = data[0]
-	}
-	result["pagination"] = data[1]
-	return status, result
-}
