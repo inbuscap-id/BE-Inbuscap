e536fe67f7e15de2d067cf0f7928ba87eae176d6
Abdul Malik <malik.the5th@gmail.com>
2024-04-15 11:39:54 +0700

    rebase

--- features/proposal/handler/handler.go
+++ features/proposal/handler/handler.go
@@ -51,7 +51,10 @@ func (ct *controller) Create() echo.HandlerFunc {
 		if err != nil {
 			return c.JSON(helper.ResponseFormat(http.StatusBadRequest, helper.ErrorUserInput))
 		}
-
+		share, err := strconv.Atoi(c.FormValue("share"))
+		if err != nil {
+			return c.JSON(helper.ResponseFormat(http.StatusBadRequest, helper.ErrorUserInput))
+		}
 		newProposal := proposal.Proposal{
 			Title:       c.FormValue("title"),
 			Description: c.FormValue("description"),
